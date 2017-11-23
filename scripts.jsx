6c86bc9cba6f7a3c1e6b82f8bd804898b05c9dc4
Ad <adunreal@gmail.com>
2017-11-23 09:59:37 +0530

    Added icons from fontawesome

--- scripts.jsx
+++ scripts.jsx
@@ -1,75 +1,75 @@
 const randomizeBoard = size => {
-  const cleanBoard = Array(size).fill(Array(size).fill(0))
+  const cleanBoard = Array(size).fill(Array(size).fill(0));
   const randomBoard = cleanBoard.map(row => {
     const newRow = row.map(cell => {
-      return Math.round(Math.random())
-    })
-    return newRow
-  })
-  return randomBoard
-}
+      return Math.round(Math.random());
+    });
+    return newRow;
+  });
+  return randomBoard;
+};
 const nextGen = cells => {
-  const newState = []
-  const totalRows = cells.length
-  const totalColumns = cells.length ? cells[0].length : 0
+  const newState = [];
+  const totalRows = cells.length;
+  const totalColumns = cells.length ? cells[0].length : 0;
   for (let row = 0; row < totalRows; row++) {
-    const newRow = []
+    const newRow = [];
     for (let col = 0; col < totalColumns; col++) {
-      let currentCell = cells[row][col]
-      let sum = currentCell
-      const upExists = row !== 0 ? true : false
-      const downExists = row !== totalRows - 1 ? true : false
-      const leftExists = col !== 0 ? true : false
-      const rightExists = col !== totalColumns - 1 ? true : false
+      let currentCell = cells[row][col];
+      let sum = currentCell;
+      const upExists = row !== 0 ? true : false;
+      const downExists = row !== totalRows - 1 ? true : false;
+      const leftExists = col !== 0 ? true : false;
+      const rightExists = col !== totalColumns - 1 ? true : false;
       if (upExists) {
-        sum += cells[row - 1][col]
+        sum += cells[row - 1][col];
       }
       if (downExists) {
-        sum += cells[row + 1][col]
+        sum += cells[row + 1][col];
       }
       if (leftExists) {
-        sum += cells[row][col - 1]
+        sum += cells[row][col - 1];
       }
       if (rightExists) {
-        sum += cells[row][col + 1]
+        sum += cells[row][col + 1];
       }
       if (upExists && rightExists) {
-        sum += cells[row - 1][col + 1]
+        sum += cells[row - 1][col + 1];
       }
       if (upExists && leftExists) {
-        sum += cells[row - 1][col - 1]
+        sum += cells[row - 1][col - 1];
       }
       if (downExists && rightExists) {
-        sum += cells[row + 1][col + 1]
+        sum += cells[row + 1][col + 1];
       }
       if (downExists && leftExists) {
-        sum += cells[row + 1][col - 1]
+        sum += cells[row + 1][col - 1];
       }
       if (sum === 3) {
-        currentCell = 1
+        currentCell = 1;
       } else if (sum === 4) {
-        currentCell = currentCell
+        currentCell = currentCell;
       } else {
-        currentCell = 0
+        currentCell = 0;
       }
-      newRow.push(currentCell)
+      newRow.push(currentCell);
     }
-    newState.push(newRow)
+    newState.push(newRow);
   }
-  return newState
-}
+  return newState;
+};
 const GameTile = ({ status, changeTile }) => {
-  const isAlive = status === 1 ? true : false
-  const cssClass = isAlive ? "tile-alive" : "tile-dead"
-  return <div className={`tile ${cssClass}`} onClick={changeTile} />
-}
+  const isAlive = status === 1 ? true : false;
+  const cssClass = isAlive ? "tile-alive" : "tile-dead";
+  return <div className={`tile ${cssClass}`} onClick={changeTile} />;
+};
 const GameRow = ({ children, size }) => {
   return (
     <div className="game-row" style={{ width: size * 16 }}>
       {children}
     </div>
-  )
-}
+  );
+};
 const createGameBoard = (arr, func) => {
   return arr.map((row, rowNumber) => {
     const newRow = row.map((tile, tileNum) => {
@@ -77,115 +77,119 @@ const createGameBoard = (arr, func) => {
         <GameTile
           status={tile}
           changeTile={() => {
-            console.log(rowNumber, tileNum)
-            func(rowNumber, tileNum)
+            console.log(rowNumber, tileNum);
+            func(rowNumber, tileNum);
           }}
         />
-      )
-    })
-    return <GameRow size={arr.length}>{newRow}</GameRow>
-  })
-}
-let playing
+      );
+    });
+    return <GameRow size={arr.length}>{newRow}</GameRow>;
+  });
+};
+let playing;
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       board: randomizeBoard(30),
       isPlaying: false,
       generation: 0,
       boardSize: 30
-    }
-    this.handleClick = this.handleClick.bind(this)
-    this.getNextGeneration = this.getNextGeneration.bind(this)
-    this.resetBoard = this.resetBoard.bind(this)
-    this.incrementSize = this.incrementSize.bind(this)
-    this.decrementSize = this.decrementSize.bind(this)
-    this.changeTile = this.changeTile.bind(this)
-    this.pauseGame = this.pauseGame.bind(this)
-    this.clear = this.clear.bind(this)
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.getNextGeneration = this.getNextGeneration.bind(this);
+    this.resetBoard = this.resetBoard.bind(this);
+    this.incrementSize = this.incrementSize.bind(this);
+    this.decrementSize = this.decrementSize.bind(this);
+    this.changeTile = this.changeTile.bind(this);
+    this.pauseGame = this.pauseGame.bind(this);
+    this.clear = this.clear.bind(this);
   }
   incrementSize() {
     if (!this.state.isPlaying) {
       this.setState(prevState => {
         return {
           boardSize: prevState.boardSize + 1
-        }
-      })
-      this.resetBoard()
+        };
+      });
+      this.resetBoard();
     }
   }
   pauseGame() {
-    if (playing) clearInterval(playing)
+    if (playing) clearInterval(playing);
     this.setState(prevState => {
-      isPlaying: !prevState.isPlaying
-    })
+      isPlaying: !prevState.isPlaying;
+    });
   }
   changeTile(row, col) {
     if (!this.state.isPlaying) {
-      const newTile = this.state.board[row][col] === 1 ? 0 : 1
-      const newRow = [...this.state.board[row]]
-      newRow.splice(col, 1, newTile)
-      const newBoard = [...this.state.board]
-      newBoard.splice(row, 1, newRow)
+      const newTile = this.state.board[row][col] === 1 ? 0 : 1;
+      const newRow = [...this.state.board[row]];
+      newRow.splice(col, 1, newTile);
+      const newBoard = [...this.state.board];
+      newBoard.splice(row, 1, newRow);
       this.setState({
         board: newBoard
-      })
+      });
     }
   }
   decrementSize() {
     if (!this.state.isPlaying) {
       this.setState(prevState => {
-        if (prevState.boardSize === 1) return {}
+        if (prevState.boardSize === 1) return {};
         return {
           boardSize: prevState.boardSize - 1
-        }
-      })
+        };
+      });
     }
-    this.resetBoard()
+    this.resetBoard();
   }
   getNextGeneration() {
     this.setState(prevState => {
       return {
         board: nextGen(prevState.board),
         generation: prevState.generation + 1
-      }
-    })
+      };
+    });
   }
   handleClick() {
     if (!this.state.isPlaying) {
-      playing = setInterval(this.getNextGeneration, 100)
+      playing = setInterval(this.getNextGeneration, 100);
       this.setState({
         isPlaying: !this.state.isPlaying
-      })
+      });
     } else {
-      clearInterval(playing)
+      clearInterval(playing);
       this.setState({
         isPlaying: false
-      })
+      });
     }
   }
   resetBoard() {
-    if (playing) clearInterval(playing)
+    if (playing) clearInterval(playing);
     this.setState({
       board: randomizeBoard(this.state.boardSize),
       generation: 0,
       isPlaying: false
-    })
+    });
   }
   clear() {
-    if (playing) clearInterval(playing)
+    if (playing) clearInterval(playing);
     this.setState({
       board: Array(this.state.boardSize).fill(
         Array(this.state.boardSize).fill(0)
       ),
       generation: 0,
       isPlaying: false
-    })
+    });
   }
   render() {
-    const gameTiles = createGameBoard(this.state.board, this.changeTile)
-    const tileText = this.state.isPlaying ? "Pause" : "Begin"
+    const gameTiles = createGameBoard(this.state.board, this.changeTile);
+    const tileText = this.state.isPlaying ? (
+      <i className="fa fa-pause" />
+    ) : (
+      <i className="fa fa-play" />
+    );
     return (
       <div id="board">
         <div id="board-container" style={{ width: this.state.boardSize * 25 }}>
@@ -193,17 +197,17 @@ class App extends React.Component {
         </div>
 
         <button onClick={this.handleClick} id="control">
-          {tileText} Simulation
+          {tileText}
         </button>
         <div id="gen">
           Gen: <span className="info">{this.state.generation}</span>
         </div>
         <div id="rc-container">
           <button id="reset" onClick={this.resetBoard}>
-            Randomize board
+            <i className="fa fa-random" />Randomize board
           </button>
           <button id="clear" onClick={this.clear}>
-            Clear board
+            <i className="fa fa-trash-o" />Clear board
           </button>
         </div>
 
@@ -217,8 +221,8 @@ class App extends React.Component {
           </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
